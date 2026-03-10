19cc0a406ff6296febd4c4c40b24f63571eb91a5
viettt0119 <viettt0119@gmail.com>
2026-03-10 09:20:08 +0700

    bài thực hành số 2 - 10/3

--- ProjectMVC/obj/Debug/net10.0/ProjectMVC.AssemblyInfo.cs
+++ ProjectMVC/obj/Debug/net10.0/ProjectMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c1ddbee83798a4d9b792ae77e6629b056ee8867")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffabaf6f2fad9b7cfd4c906c39dc805d2789bc02")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
