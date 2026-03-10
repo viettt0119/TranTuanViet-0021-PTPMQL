ffabaf6f2fad9b7cfd4c906c39dc805d2789bc02
viettt0119 <viettt0119@gmail.com>
2026-03-10 08:54:49 +0700

    update data baithuchanh so 2

--- ProjectMVC/obj/Debug/net10.0/ProjectMVC.AssemblyInfo.cs
+++ ProjectMVC/obj/Debug/net10.0/ProjectMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de86cf0ca99a330c0202fbcddb3e60b08b92d6bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c1ddbee83798a4d9b792ae77e6629b056ee8867")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
